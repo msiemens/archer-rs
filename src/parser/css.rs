bf061bf99dccfd29b5b5e037074a257b0e967c88
Markus Siemens <markus@m-siemens.de>
2016-01-22 20:08:57 +0700

    Add basic URL rewrriting for CSS

--- src/parser/css.rs
+++ src/parser/css.rs
@@ -1,10 +1,11 @@
+use std::borrow::Cow;
 use std::io::Read;
 
 use cssparser::{Parser, Token};
 use url::Url;
 
 use Queues;
-use parser::resolve_rel_url;
+use parser::{resolve_rel_url, rewrite_url};
 
 
 pub fn explore_css<R: Read>(data: &mut R, base: &Url, _: Queues) -> Vec<Url> {
@@ -15,12 +16,17 @@ pub fn explore_css<R: Read>(data: &mut R, base: &Url, _: Queues) -> Vec<Url> {
 
     // TODO: Store rewritten CSS
 
-    parse_css(&mut parser, base).unwrap()
+    let (urls, tokens) = parse_css(&mut parser, base).unwrap();
+
+    urls
 }
 
 
-fn parse_css<'i, 't>(parser: &mut Parser<'i, 't>, base: &Url) -> Result<Vec<Url>, ()> {
+fn parse_css<'i, 't>(parser: &mut Parser<'i, 't>,
+                     base: &Url)
+                     -> Result<(Vec<Url>, Vec<Token<'i>>), ()> {
     let mut urls = Vec::new();
+    let mut tokens = Vec::new();
 
     while !parser.is_exhausted() {
         let token = match parser.next() {
@@ -34,7 +40,10 @@ fn parse_css<'i, 't>(parser: &mut Parser<'i, 't>, base: &Url) -> Result<Vec<Url>
             Token::CurlyBracketBlock |
             Token::SquareBracketBlock => {
                 parser.parse_nested_block(|parser| {
-                          parse_css(parser, base).map(|new_urls| urls.extend(new_urls.into_iter()))
+                          parse_css(parser, base).map(|(new_urls, new_tokens)| {
+                              urls.extend(new_urls.into_iter());
+                              tokens.extend(new_tokens.into_iter())
+                          })
                       })
                       .unwrap();
             }
@@ -43,9 +52,15 @@ fn parse_css<'i, 't>(parser: &mut Parser<'i, 't>, base: &Url) -> Result<Vec<Url>
 
         trace!("Processing CSS token {:?}", token);
 
+        let mut add_tokens_cb: Option<Box<FnMut(String)>> = None;
+
         let url = match token {
             Token::UnquotedUrl(url) => {
                 debug!("Found URL in CSS: {}", url);
+
+                add_tokens_cb = Some(Box::new(|rewritten| {
+                    tokens.push(Token::UnquotedUrl(Cow::Owned(rewritten)));
+                }));
 
                 url
             }
@@ -53,25 +68,40 @@ fn parse_css<'i, 't>(parser: &mut Parser<'i, 't>, base: &Url) -> Result<Vec<Url>
                 match parser.expect_url_or_string() {
                     Ok(url) => {
                         debug!("Found @import in CSS: {}", url);
+
+                        add_tokens_cb = Some(Box::new(|rewritten| {
+                            tokens.push(Token::AtKeyword(Cow::Borrowed("import")));
+                            tokens.push(Token::UnquotedUrl(Cow::Owned(rewritten)))
+                        }));
+
                         url
                     }
                     Err(..) => continue,
                 }
             }
-            _ => continue,
+            _ => {
+                tokens.push(token);
+                continue;
+            }
         };
 
         match resolve_rel_url(base, &url) {
-            Some(url) => urls.push(url),
+            Some(url) => {
+                if let Some(mut cb) = add_tokens_cb {
+                    cb(rewrite_url(&url))
+                };
+
+                urls.push(url);
+            }
             None => {
                 warn!("Failed to resolve <base href=\"{}\"> with respect to {}",
                       url,
-                      base)
+                      base);
             }
         }
     }
 
-    Ok(urls)
+    Ok((urls, tokens))
 }
 
 
